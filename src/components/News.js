beff3a7ee89dd0bdfdae1e60e0614e3a34381844
Ramanjs <raman.s2662@gmail.com>
2022-12-01 16:35:39 +0530

    add: all files

---
+++ src/components/News.js
@@ -0,0 +1,55 @@
+import React, {useEffect, useState} from 'react'
+
+import NewsItem from './NewsItem'
+import PropTypes from 'prop-types'
+
+const News = (props)=>{
+    const [articles, setArticles] = useState([])
+    const [page, setPage] = useState(1)
+    const [totalResults, setTotalResults] = useState(0)
+    
+    const updateNews = async ()=> {
+        const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`; 
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        setArticles(parsedData.articles)
+        setTotalResults(parsedData.totalResults)
+    }
+
+    useEffect(() => {
+        document.title = props.category + `- GDSC News`;
+        updateNews(); 
+        // eslint-disable-next-line
+    }, [])
+
+        return (
+            <>
+                <h1 className="text-center" style={{ margin: '35px 0px', marginTop: '90px' }}>GDSC News - Top {props.category} Headlines</h1>
+                   <div className="container">
+                    <div className="row">
+                        {articles.map((element) => {
+                            return <div className="col-md-4" key={element.url}>
+                                <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
+                            </div>
+                        })}
+                    </div>
+                    </div> 
+            </>
+        )
+    
+}
+
+
+News.defaultProps = {
+    country: 'in',
+    pageSize: 8,
+    category: 'general',
+}
+
+News.propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+}
+
+export default News
